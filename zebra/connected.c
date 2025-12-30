77488e2eb1aaa1a006e1a9542ce1054aea3746a3
ne-vlezay80 <ne-vlezay80@yandex.ru>
2025-12-31 00:07:21 +0300

    zebra: fix duplicate route from local protocol
    The change is fixing duplicate route from local protocol, if interface change attribute. (replacing function route_node_match() to route_node_lookup())
    
    Signed-off-by: Alexey Boldyrev <ne-vlezay80@yandex.ru>
    
    Signed-off-by: ne-vlezay80 <ne-vlezay80@yandex.ru>

--- zebra/connected.c
+++ zebra/connected.c
@@ -343,8 +343,8 @@ void connected_up(struct interface *ifp, struct connected *ifc)
 	struct route_table *m_table = zebra_vrf_table(afi, SAFI_MULTICAST, zvrf->vrf->vrf_id);
 
 	if (install_local) {
-		struct route_node *u_rn = route_node_match(u_table, &plocal);
-		struct route_node *m_rn = route_node_match(m_table, &plocal);
+		struct route_node *u_rn = route_node_lookup(u_table, (struct prefix *)&plocal);
+		struct route_node *m_rn = route_node_lookup(m_table, (struct prefix *)&plocal);
 
 		if (u_rn) {
 			rib_delete(afi, SAFI_UNICAST, zvrf->vrf->vrf_id, ZEBRA_ROUTE_LOCAL, 0, 0,
